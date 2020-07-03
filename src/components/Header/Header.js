b4a13b2fc10f2bf2512f9baffa36bb4dd0416cbd
Mikita D <ndosyukov@gmail.com>
2020-07-03 01:59:53 -0700

    implemented sliding hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from 'react';
 import './Header.css';
 
 function Header() {
+    const [isActive, setIsActive] = useState(false);
 
     useEffect(() => {
         window.addEventListener('resize', () => console.log(window.innerWidth))
+        console.log('isActive', isActive)
         return () => {
-            window.removeEventListener('resize')
+            window.removeEventListener('resize', () => window.innerWidth)
         }
     })
 
@@ -22,13 +24,33 @@ function Header() {
                         <a href="#">Sign In</a>
                         <a href="#">Bag<span className="dot"></span></a>
                     </div>
-                    <div className="hamburger-wrapper">
+                    <a className="hidden-bag" href="#">Bag<span className="dot"></span></a>
+                    <div onClick={() => setIsActive(prevState => prevState = !prevState)} className="hamburger-wrapper">
                         <div className="hamburger-menu">
                         </div>
                     </div>
                 </div>
             </header>
-        </React.Fragment>
+
+            <div style={{ transform: isActive ? 'translate(0%)' : 'translate(100%)' }} className="nav-bar-slider">
+                <div className="nav-bar-slider-body">
+                    <ul className="slider-links">
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><div className="divider"></div></li>
+                        <li><a href="#">Bag</a></li>
+                        <li><div className="divider"></div></li>
+                        <li><a className="social-link" href="#">Facebook</a></li>
+                        <li><a className="social-link" href="#">Instagram</a></li>
+                    </ul>
+
+                </div>
+                <div className="nav-bar-slider-footer">
+                    <button className="sign-in-up-button">Log In / Register</button>
+                </div>
+            </div>
+
+        </React.Fragment >
     )
 }
 
